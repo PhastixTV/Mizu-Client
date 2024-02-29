d3d0312c881c444da23a31e27f3f9c04df7b764a
PhastixTV <philipp.sidler0206@gmail.com>
2024-02-29 16:10:02 +0100

    init commit

---
+++ src/main/java/dev/phastixtv/MizuClient.java
@@ -0,0 +1,10 @@
+package dev.phastixtv;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MizuClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
